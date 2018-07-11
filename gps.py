41078983be28fd1b51f481f6f92e058e717aadd7
Steven Shan <sshan@FM.local>
2018-07-11 18:12:05 -0400

    beggining bfs

--- gps.py
+++ gps.py
@@ -20,4 +20,17 @@ if __name__ == "__main__":
 	start_index = coord2index(start_x, start_y)
 	end_index = coord2index(end_x, end_y)
 
-	
+	print("Finding route from (" + str(start_x) + ", " + str(start_y) + 
+		") to (" + str(end_x) + ", " + str(end_y) + ")")
+	print("Start index: " + str(start_index))
+	print("End index: " + str(end_index))
+
+	# run BFS
+	queue = [start_index]
+	visited = [False] * (SIZE * SIZE)
+	visited[start_index] = True
+
+	while len(queue) > 0:
+		tempQueue = []
+		for position in queue:
+			_tempQueue = adjList[visited]
